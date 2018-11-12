8b45e35e86686ec6ae96a12d61ca45663c81e6d7
Your Name <you@example.com>
2018-11-12 21:31:50 +0800

    tijiao

---
+++ note/week-3/js/nav-data.js
@@ -0,0 +1,67 @@
+var navdata = [
+	[
+		{
+			title:'新品',
+			img:'img/pro-m1.png',
+			name:'小米MIX 3',
+			price:'3299元起'
+		},
+		{
+			title:'新品',
+			img:'img/pro-m2.png',
+			name:'小米8 青春版',
+			price:'3299元起'
+		},
+		{
+			title:'新品',
+			img:'img/pro-m3.png',
+			name:'小米8 屏幕指纹版',
+			price:'3299元起'
+		},
+		{
+			img:'img/pro-m4.png',
+			name:'小米8',
+			price:'3299元起'
+		},
+		{
+			img:'img/pro-m5.png',
+			name:'小米8 SE',
+			price:'3299元起'
+		},
+		{
+			img:'img/pro-m6.png',
+			name:'小米MAX 3',
+			price:'3299元起'
+		},
+	],
+	[
+		{
+			title:'新品',
+			img:'img/pro-m1.png',
+			name:'红米6 pro',
+			price:'999元起'
+		},
+		{
+			title:'新品',
+			img:'img/pro-m2.png',
+			name:'红米6',
+			price:'799元起'
+		},
+		{
+			title:'新品',
+			img:'img/pro-m3.png',
+			name:'红米6A',
+			price:'599元起'
+		},
+		{
+			img:'img/pro-m4.png',
+			name:'红米Note5',
+			price:'1099元起'
+		},
+		{
+			img:'img/pro-m5.png',
+			name:'红米S2',
+			price:'999元起'
+		},
+	]
+];
