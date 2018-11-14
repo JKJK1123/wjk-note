5898cc69f3e65f38a2a91bd51976eaefdbb7ef90
Your Name <you@example.com>
2018-11-14 21:18:49 +0800

    xiugaile

---
+++ note/week-4/BOM操作.md
@@ -0,0 +1,9 @@
+### offet
+    1. offsetWith 
+    2. offsetHeight
+    * 包含 border 和 padding ,指的是元素实际大小
+    3. offsetLeft
+    4. offsetTop
+    * offsetLeft/offsetTop和定位的absolute属性的表现形式一样,如果父类(父类的父类..)有定位元素,则找父类,如果都没有,找body
+    5. offsetParent
+     找到当前元素偏移参照的父类元素
---
+++ word/3/js/nav2-data.js
@@ -0,0 +1,106 @@
+var nav2Data = [
+	[
+		{
+			img: 'img/k-m1.png',
+			name: '小米MIX 3'
+		},
+		{
+			img: 'img/k-m2.png',
+			name: '小米MIX 3'
+		},
+		{
+			img: 'img/k-m3.png',
+			name: '小米MIX 3'
+		},
+		{
+			img: 'img/k-m4.png',
+			name: '小米MIX 3'
+		},
+		{
+			img: 'img/k-m5.png',
+			name: '小米MIX 3'
+		},
+		{
+			img: 'img/k-m6.png',
+			name: '小米MIX 3'
+		}
+	],
+	[	
+		{
+			img: 'img/k-m1.png',
+			name: '小米MIX 3'
+		},
+		{
+			img: 'img/k-m2.png',
+			name: '小米MIX 3'
+		},
+		{
+			img: 'img/k-m3.png',
+			name: '小米MIX 3'
+		},
+		{
+			img: 'img/k-m4.png',
+			name: '小米MIX 3'
+		},
+		{
+			img: 'img/k-m5.png',
+			name: '小米MIX 3'
+		},
+		{
+			img: 'img/k-m6.png',
+			name: '小米MIX 3'
+		}
+	],
+	[	
+		{
+			img: 'img/k-m1.png',
+			name: '小米MIX 3'
+		},
+		{
+			img: 'img/k-m2.png',
+			name: '小米MIX 3'
+		},
+		{
+			img: 'img/k-m3.png',
+			name: '小米MIX 3'
+		},
+		{
+			img: 'img/k-m4.png',
+			name: '小米MIX 3'
+		},
+		{
+			img: 'img/k-m5.png',
+			name: '小米MIX 3'
+		},
+		{
+			img: 'img/k-m6.png',
+			name: '小米MIX 3'
+		}
+	],
+	[	
+		{
+			img: 'img/k-m1.png',
+			name: '小米MIX 3'
+		},
+		{
+			img: 'img/k-m2.png',
+			name: '小米MIX 3'
+		},
+		{
+			img: 'img/k-m3.png',
+			name: '小米MIX 3'
+		},
+		{
+			img: 'img/k-m4.png',
+			name: '小米MIX 3'
+		},
+		{
+			img: 'img/k-m5.png',
+			name: '小米MIX 3'
+		},
+		{
+			img: 'img/k-m6.png',
+			name: '小米MIX 3'
+		}
+	]
+];
